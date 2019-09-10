60fd347925a70311182af6240eb0620c0c5781ac
Lauren Wong <lauren@schooldebian.localhost>
2019-09-09 22:05:17 -0700

    add clarifying comments and fix formatting

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
@@ -17,12 +17,12 @@ public class Evaluator {
     public Evaluator() {
         operandStack = new Stack<>();
         operatorStack = new Stack<>();
+
     }
 
     public int eval(String expression) {
         String token;
         int parens=0;
-        System.out.println(expression);
         // The 3rd argument is true to indicate that the delimiters should be used
         // as tokens, too. But, we'll need to remember to filter out spaces.
         this.tokenizer = new StringTokenizer(expression, DELIMITERS, true);
@@ -46,67 +46,66 @@ public class Evaluator {
                     // and values will be instances of the Operators.  See Operator class
                     // skeleton for an example.
                     Operator newOperator = Operator.getOperator(token);
+                    //if operator stack is empty push newOperator and go directly to checking next token
                     if(operatorStack.isEmpty()){
                         operatorStack.push(newOperator);
                         continue;
                     }
-                    if (newOperator.priority()==4){
+                    //if operator is "(" push it to stack and go directly to checking next token
+                    else if (newOperator.priority() == 4){
                         operatorStack.push(newOperator);
                         continue;
                     }
 
-                    else if (newOperator.priority()==5){
+                    //if operator is ")" execute everything on stacks until "("
+                    else if (newOperator.priority() == 5){
 
-                        while (!operatorStack.isEmpty() && operatorStack.peek().priority()!=4){
+                        while (!operatorStack.isEmpty() && operatorStack.peek().priority() !=4 ){
 
-                                    System.out.println(newOperator.priority());
                                     Operator oldOpr = operatorStack.pop();
                                     Operand op2 = operandStack.pop();
                                     Operand op1 = operandStack.pop();
                                    operandStack.push(oldOpr.execute(op1, op2));
 
-
-
-
                             }
-                        //pop parens
-                        if (!operatorStack.isEmpty()){
+
+                        //pop "("
                             operatorStack.pop();
 
                         }
-                        }
 
-
+                        //otherwise execute operations according to priority
+                        //push newOperator onto stack once:
+                        //stack is empty
+                        //top of stack is "("
+                        //it is higher priority than top of stack
                    else try {
-                        while (operatorStack.peek().priority() >= newOperator.priority() && operatorStack.peek().priority()!=4) {
+                        while (operatorStack.peek().priority() >= newOperator.priority() && operatorStack.peek().priority() != 4) {
+
                             Operator oldOpr = operatorStack.pop();
                             Operand op2 = operandStack.pop();
                             Operand op1 = operandStack.pop();
-                            operandStack.push(oldOpr.execute(op1, op2));}
-                        operatorStack.push(newOperator);
+                            operandStack.push(oldOpr.execute(op1, op2));
+                        }
+                            operatorStack.push(newOperator);
                     }
                     catch (EmptyStackException e) {
                         operatorStack.push(newOperator);
-                        continue;
                     }
                 }
             }
         }
 
 
-        // Control gets here when we've picked up all of the tokens; you must add
-        // code to complete the evaluation - consider how the code given here
-        // will evaluate the expression 1+2*3
-        // When we have no more tokens to scan, the operand stack will contain 1 2
-        // and the operator stack will have + * with 2 and * on the top;
-        // In order to complete the evaluation we must empty the stacks,
-        // that is, we should keep evaluating the operator stack until it is empty;
-        // Suggestion: create a method that processes the operator stack until empty.
-
-        //Don't forget to change the return value!
         int result=0;
         Operand op1, op2;
         Operator thisOperator;
+        //if operator stack is empty the whole expression must have been in parens.
+        //the result is just whatever is on the operand stack.
+        if (operatorStack.isEmpty()){
+            result=operandStack.pop().getValue();
+        }
+        //execute remaining stack
         while(!operatorStack.isEmpty())
         {
 
@@ -114,12 +113,10 @@ public class Evaluator {
             op1=operandStack.pop();
             thisOperator=operatorStack.pop();
             result = thisOperator.execute(op1,op2).getValue();
-            System.out.println(op1.getValue()+ " "+ thisOperator.print() +" " + op2.getValue()+ "=" + result);
             operandStack.push(thisOperator.execute(op1,op2));
 
-
-
         }
+
 
 
         return result;
